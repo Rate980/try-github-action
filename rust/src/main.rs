c82984bf0bb648d28b6eb852cb7e794f99e63d5b
Rate980 <a@a>
2023-11-09 14:56:09 +0900

    add test

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_pass() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_fail() {
+        assert_eq!(2 + 2, 5);
+    }
+}
